6dfb7a35ab6faa48353c4dd15fc617f512896f82
AbdulBaseer7896 <abdulbasirqazi7896@gmail.com>
2025-09-18 12:48:15 +0500

    now the basic app is ready

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './index.css'
+import React from 'react'
+function App() {
+
+  return (
+    <>
+      <p className='text-4xl font-bold '>Allah is Great</p>
+    </>
+  )
+}
+
+export default App
